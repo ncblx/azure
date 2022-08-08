afbebd6e98734b003c7be161ff17b994f46635e9
Emin <user084@gmail.com>
2022-08-08 22:47:11 +0200

    Update

--- powershell/main.ps1
+++ powershell/main.ps1
@@ -7,71 +7,96 @@ Param(
     [Parameter(Mandatory = $false)] [string]$spId,
     [Parameter(Mandatory = $false)] [string]$group,
     [Parameter(Mandatory = $false)] [string]$gatewayName,
-    [Parameter(Mandatory = $false)] [string]$reportConfigFile,
+    [Parameter(Mandatory = $false)] [string]$freportConfigFile,
     [Parameter(Mandatory = $false)] [string]$reportDirectory,
-    [Parameter(Mandatory = $false)] [string]$AddSchedule,
-    [Parameter(Mandatory = $false)] [string]$scheduleIson,
-    [switch]$addUserToworskpace = $false,
+    [parameter(Mandatory = $false)] [string]$DatasetName,
+    [parameter(Mandatory = $false)] [string]$Parameterlist,
+    [switch]$addUserToWorskpace = $false,
     [switch]$addSPToWorskpace = $false,
     [switch]$addGroupToWorskpace = $false,
-    [switch]$updateteway = $false,
-    [switch]$uploadReport = $false
+    [switch]$updateGteway = $false,
+    [switch]$uploadReport = $false,
+    [switch]$updatePowerBIDatasetParameters = $false,
+    [switch]$rebindReports = $false
 )
 
+if (condition) {
+    <# Action to perform if the condition is true #>
+}
+
 if ($null -eq (Get-InstalledModule `
-            -Name "Microsoft PowerBIMgmt. Admin" `
+            -Name "MicrosoftPowerBIMgmt.Admin" `
             -ErrorAction SilentlyContinue)) {
-    Write-Host "nMicrosoft PowerBIMgmt Admin does not installed"
-    Install-Module -Name MicrosoftPowerBIMgmt.Admin -Scope CurrentUser -Force
+    Write-Host "'MicrosoftPowerBIMgmt. Admin does not installed"
+    Install-Module-Name MicrosoftPowerBIMgmt. Admin-Scope CurrentUser-Force
 }
 else {
-    Write-Host "nMicrosoft PowerBIMgmt.Admin installed"
+    Write-Host " nMicrosoftPowerBIMgmt. Admin installed"
 }
 
-Import-Module -Name $PSScriptRoot\setupPowerBI.psm1 -Force `
-    -Argumentlist $configfile
 
-# Set the values from the config file passed as the argument
+Import-Module-Name $P5ScriptRoot\setupPowerBI.psml-Force `
+    -ArgumentList SconfigFile
+
+#Set the values from the config file passed as the argument
 $settings = readConfig $configFile
 $tenantId = $settings.azure.tenantId
 # $subscriptionName = $settings.azure.subscriptionName
-# $subscriptionId = $settings.azure.subscriptionid
-# $clientId = $settings.azure.clientid
-$powerbiurl = $settings.azure.powerbiUrl
-$cloudEnv = $settings.environment.cloudEnv
-$vmkeyVaultName = = $settings.azure.vmkeyVaultName
-$keyVaultScuser = $settings.azure.keyVaultsScUser
-$keyVaultScSecret = $settings.azure.keyVaultScSecret
-# $scheduleJson = $settings.schedule
+# $subscriptionId = $settings.azure.subscriptionId
+# $clientId = $settings.azure.clientId
+$powerbiUr1 = $settings.azure.powerbiUrl
+$ubscloudEnv = $settings.environment.ubscloudEnv
+$vmKeyVaultName = $settings.azure.vmKeyVaultName
+$keyVaultScUser = $settings.azure.keyVaultScUser
+#keyVaultScSecret = $settings. azure. keyVaultScSecret
 
-Import-Module -Name $PSScriptRoot\managePowerBI.psm1 -Force `
-    - Argumentlist $tenantId, $powerbiurl, $userEmail, $workspaceName, $accessRight, $spId, `
-    $group, $gatewayName, $cloudEnv, $reportConfigfile, $reportDirectory, $vmkeyVaultName, `
-    $keyVaultScuser, $keyVaultScSecret, $AddSchedule, $scheduleJson
+
+
+Import-Module-Name $PSScriptRoot\managePowerBI.psm1 -Force `
+    -ArgumentList $tenantId, $powerbiUr1, SuserEmail, SworkspaceName, $accessRight, $spId, $group, $gatewayName, $ubscloudEnv, $reportConfigFile, $reportDirectory, $vmKeyVaultName, $keyVaultScUser, $keyVaultScSecret,
+$DatasetName, $Parameterlist
+
+
 
 if (!(
-    $addUserToworskpace -or
-    $addSPToWorskpace -or
-    $addGroupToWorskpace -or
-    $updateGteway -or
-    $uploadReport
-))
-
+        $addUserToWorskpace -or
+        $addSPToWorskpace -or
+        $addGroupToWorskpace -or
+        $updateGteway -or
+        $uploadReport -or
+        $updatePowerBIDatasetParameters -or
+        $rebindReports
+    ))
 {
     throw "Please select valid option for the Deployment"
 }
+    
+    
+    
 if ($addUserToWorskpace) {
-    addUserToworskpace
+    addUserToWorskpace
 }
+
 if ($addSPToWorskpace) {
     addSPToWorskpace
 }
+    
+    
 if ($addGroupToWorskpace) {
-        addGroupToWorskpace
+    addGroupToWorskpace
 }
+
 if ($updateGteway) {
-    updateteway
+    updateGteway
 }
+
 if ($uploadReport) {
     uploadReport
+}
+
+if ($updatePowerBIDatasetParameters) {
+    updatePowerBIDatasetParameters
+}
+if ($rebindReports) {
+    rebindReports
 }
